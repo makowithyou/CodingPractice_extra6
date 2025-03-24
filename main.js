0d6f30be2038f170d03f1c22a77b4208c6e903c2
makowithyou <mako.withyou@gmail.com>
2025-03-24 14:06:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const MainImg = document.getElementById("MainImg");
+MainImg.setAttribute("src", "https://github.com/makowithyou/CodingPractice_extra6/blob/main/img/mainvisual1.jpg?raw=true");
+MainImg.setAttribute("alt", "mainvisual1");
+
+addEventListener(
+    "mouseover", (e) => {
+        if (e.target.className == "thumbnail") {
+            const src = e.target.getAttribute("src");
+            const alt = e.target.getAttribute("alt");
+            MainImg.setAttribute("src", src);
+            MainImg.setAttribute("alt", alt);
+        }
+    }
+)
