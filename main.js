6db290bf1d70ef91a97c300fc3772cb29bc1c75d
makowithyou <mako.withyou@gmail.com>
2025-03-25 13:38:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const MainImg = document.getElementById("MainImg");
-
 addEventListener(
     "mouseover", (e) => {
         if (e.target.className == "thumbnail") {
             MainImg.classList.toggle("fadeout");
-            const src = e.target.getAttribute("src");
-            const alt = e.target.getAttribute("alt");
-            MainImg.setAttribute("src", src);
-            MainImg.setAttribute("alt", alt);
+            new Promise((resolve)=> {
+                setTimeout(()=>{
+                    const src = e.target.getAttribute("src");
+                    const alt = e.target.getAttribute("alt");
+                    MainImg.setAttribute("src", src);
+                    MainImg.setAttribute("alt", alt);
+                    resolve();
+                },500);
+            }).then(()=>{
+                MainImg.classList.toggle("fadeout");
+            });
         }
     }
 )
