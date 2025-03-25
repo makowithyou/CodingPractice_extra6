7f0b191a0e7c2a6a0c19076097d32b19d514d712
makowithyou <mako.withyou@gmail.com>
2025-03-25 14:46:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const MainImg = document.getElementById("MainImg");
-addEventListener(
-    "mouseover", (e) => {
-        if (e.target.className == "thumbnail") {
-            MainImg.classList.toggle("fadeout");
-            new Promise((resolve)=> {
-                setTimeout(()=>{
-                    const src = e.target.getAttribute("src");
-                    const alt = e.target.getAttribute("alt");
-                    MainImg.setAttribute("src", src);
-                    MainImg.setAttribute("alt", alt);
-                    resolve();
-                },500);
-            }).then(()=>{
-                MainImg.classList.toggle("fadeout");
-            });
-        }
+// const MainImg = document.getElementById("MainImg");
+// addEventListener(
+//     "mouseover", (e) => {
+//         if (e.target.className == "thumbnail") {
+//             MainImg.classList.toggle("fadeout");
+//             new Promise((resolve)=> {
+//                 setTimeout(()=>{
+//                     const src = e.target.getAttribute("src");
+//                     const alt = e.target.getAttribute("alt");
+//                     MainImg.setAttribute("src", src);
+//                     MainImg.setAttribute("alt", alt);
+//                     resolve();
+//                 },500);
+//             }).then(()=>{
+//                 MainImg.classList.toggle("fadeout");
+//             });
+//         }
+//     }
+// )
+
+
+// jquery
+$(".thumbnail").hover(
+    function() {
+        const src = $(this).attr('src');
+        const alt = $(this).attr('alt');
+        $("#MainImg").fadeOut();
+        $("#MainImg").promise().done(function() {
+            $(this).attr("src", src);
+            $(this).attr("alt", alt);
+        });
+        $("#MainImg").promise().done(function() {
+            $("#MainImg").fadeIn();
+        });
     }
-)
+);
