0614d5999e844fbd763483c331743cf015d4038e
makowithyou <mako.withyou@gmail.com>
2025-03-28 11:07:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ addEventListener(
         if (e.target.className == "thumbnail") {
             const src = e.target.getAttribute("src");
             const alt = e.target.getAttribute("alt");
-            MainImg.classList.toggle("fadeout");
-            setTimeout(() => {
-                MainImg.setAttribute("src", src);
-                MainImg.setAttribute("alt", alt);
+            if(MainImg.getAttribute("src") != src) {
                 MainImg.classList.toggle("fadeout");
-            }, 500);
+                setTimeout(() => {
+                    MainImg.setAttribute("src", src);
+                    MainImg.setAttribute("alt", alt);
+                    MainImg.classList.toggle("fadeout");
+                }, 500);
+            }
         }
     }
 )
 
 
 // jquery
-// $(".thumbnail").mouseover(
-//     function() {
-//         const src = $(this).attr('src');
-//         const alt = $(this).attr('alt');
-//         $("#MainImg").addClass("fadeout");
-//         setTimeout(() => {
-//             $("#MainImg").attr("src", src);
-//             $("#MainImg").attr("alt", alt);
-//             $("#MainImg").removeClass("fadeout");
-//         }, 500);
+// $(".thumbnail").mouseover(function() {
+//     const src = $(this).attr('src');
+//     const alt = $(this).attr('alt');
+//     if ($("#MainImg").attr("src") != src){
+//         $("#MainImg").fadeOut(500, function(){
+//             $("#MainImg").attr({src: src, alt: alt});
+//             $("#MainImg").fadeIn(500);
+//         })
 //     }
-// );
+// });
